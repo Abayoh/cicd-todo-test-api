3f243d9ed3c52c58f0bd399fa443964517afea46
abayoh <alexanderbayoh@hotmail.com>
2024-06-05 15:02:28 +0000

    made some changes to see if jenkins is working

--- src/config/dbConfig.ts
+++ src/config/dbConfig.ts
@@ -1,18 +1,18 @@
 import mongoose from 'mongoose';
 
-  
 export default async () => {
-    try {
-      const uri = process.env.MONGODB_URI
-      const dbName = process.env.MONGODB_NAME
-      const username = process.env.MONGODB_USERNAME
-        const password = process.env.MONGODB_PASSWORD
-        const cluster = process.env.MONGODB_CLUSTER
-        const url = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
-        const conn = await mongoose.connect(url);
-        console.log(`MongoDB connected: ${conn.connection.host}`)
-    } catch (error) {
-        console.error(error);
-        process.exit(1);
-    }
-}
+  try {
+    const uri = process.env.MONGODB_URI;
+    const dbName = process.env.MONGODB_NAME;
+    const username = process.env.MONGODB_USERNAME;
+    const password = process.env.MONGODB_PASSWORD;
+    console.log('password', password);
+    const cluster = process.env.MONGODB_CLUSTER;
+    const url = `mongodb+srv://${username}:${password}@${cluster}/${dbName}?retryWrites=true&w=majority`;
+    const conn = await mongoose.connect(url);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
